46551f83b8e117af080bab1431182f453a66d135
lbwa <mr_lbw@outlook.com>
2019-11-23 16:44:14 +0800

    feat: support readableStream with ws.send

--- src/server-ws/index.ts
+++ src/server-ws/index.ts
@@ -1,6 +1,8 @@
 import WebSocket from 'ws'
 import http from 'http'
 import { randomBytes } from 'crypto'
+import fs from 'fs'
+import path from 'path'
 
 interface RequestAction {
   action: number
@@ -15,6 +17,8 @@ export function createWebSocketServer(options: WebSocket.ServerOptions) {
     req: http.IncomingMessage,
     client: Set<WebSocket>
   ) {
+    detectRemoteIP(req)
+
     ws.on('message', function onmessage(data: string) {
       let request: RequestAction
       try {
@@ -35,8 +39,8 @@ export function createWebSocketServer(options: WebSocket.ServerOptions) {
 }
 
 function router(ws: WebSocket, request: RequestAction) {
-  const send = (res: any, cb?: (err?: Error | undefined) => void) =>
-    ws.send(JSON.stringify(res), cb)
+  const send = (...rest: [Record<string, any>, ...any[]]) =>
+    ws.send(JSON.stringify(rest[0]), ...rest.slice(1))
 
   switch (request.action) {
     case 0:
@@ -47,7 +51,34 @@ function router(ws: WebSocket, request: RequestAction) {
 
     case 1:
       // https://nodejs.org/api/crypto.html#crypto_crypto_randombytes_size_callback
+      // https://github.com/websockets/ws/blob/master/doc/ws.md#websocketsenddata-options-callback
       ws.send(randomBytes(1024))
+      return
+
+    case 2:
+      const rs = fs.createReadStream(
+        path.resolve(process.cwd(), 'src/services/video.mp4'),
+        {
+          // DOC: https://nodejs.org/api/stream.html#stream_buffering
+          highWaterMark: 1 * 1024 // bytes
+        }
+      )
+      let size = 0
+      let chunks = 0
+      rs.on('data', chunk => {
+        chunks++
+        size += chunk.length
+        ws.send(chunk)
+      })
+      rs.on('end', () => {
+        send({
+          type: 0,
+          data: {
+            size,
+            chunks
+          }
+        })
+      })
       return
 
     default:
@@ -56,4 +87,10 @@ function router(ws: WebSocket, request: RequestAction) {
         data: 'Bad request'
       })
   }
+}
+
+function detectRemoteIP(req: http.IncomingMessage) {
+  const ip = req.connection.remoteAddress
+  console.info(`[IP]: Remote IP: ${ip || 'Unknown'}`)
+  return ip
 }
