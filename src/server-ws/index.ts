bc4f415f5b34e20fc68b4d92b544f4216ecbcb26
lbwa <mr_lbw@outlook.com>
2019-11-23 16:50:26 +0800

    feat: extract web socket router

--- src/server-ws/index.ts
+++ src/server-ws/index.ts
@@ -1,10 +1,9 @@
 import WebSocket from 'ws'
 import http from 'http'
-import { randomBytes } from 'crypto'
-import fs from 'fs'
-import path from 'path'
 
-interface RequestAction {
+import { router } from './router'
+
+export interface RequestAction {
   action: number
   [key: string]: any
 }
@@ -36,57 +35,6 @@ export function createWebSocketServer(options: WebSocket.ServerOptions) {
       router(ws, request)
     })
   })
-}
-
-function router(ws: WebSocket, request: RequestAction) {
-  const send = (...rest: [Record<string, any>, ...any[]]) =>
-    ws.send(JSON.stringify(rest[0]), ...rest.slice(1))
-
-  switch (request.action) {
-    case 0:
-      return send({
-        type: 0,
-        data: 'connected'
-      })
-
-    case 1:
-      // https://nodejs.org/api/crypto.html#crypto_crypto_randombytes_size_callback
-      // https://github.com/websockets/ws/blob/master/doc/ws.md#websocketsenddata-options-callback
-      ws.send(randomBytes(1024))
-      return
-
-    case 2:
-      const rs = fs.createReadStream(
-        path.resolve(process.cwd(), 'src/services/video.mp4'),
-        {
-          // DOC: https://nodejs.org/api/stream.html#stream_buffering
-          highWaterMark: 1 * 1024 // bytes
-        }
-      )
-      let size = 0
-      let chunks = 0
-      rs.on('data', chunk => {
-        chunks++
-        size += chunk.length
-        ws.send(chunk)
-      })
-      rs.on('end', () => {
-        send({
-          type: 0,
-          data: {
-            size,
-            chunks
-          }
-        })
-      })
-      return
-
-    default:
-      return send({
-        type: 1,
-        data: 'Bad request'
-      })
-  }
 }
 
 function detectRemoteIP(req: http.IncomingMessage) {
